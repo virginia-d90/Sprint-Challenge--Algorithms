584ece204ef9f8ffb4eac39fe0ac5ba0715aea77
Virginia Davenport <virginialdport@gmail.com>
2020-08-21 10:15:42 -0600

    count_th working

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -5,6 +5,13 @@ Your function must utilize recursion. It cannot contain any loops.
 '''
 def count_th(word):
     
-    # TBC
-    
-    pass
+    #Base case if word is 0 characters or 1 character return 0
+    if len(word) < 2:
+        return 0
+
+    #Check if first two characters are equal --> recursively call to increment along 'word' one character at a time
+    # add 1 for each occurance of 'th'
+    if word[:2] == 'th':
+        return 1 + count_th(word[2:])
+    else:
+        return count_th(word[1:])
